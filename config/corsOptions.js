5f1040e9b771046d95184ff3773a7187525d78bb
Anish Mulla <106917595+Anishm079@users.noreply.github.com>
2023-04-16 12:51:55 +0530

    Update corsOptions.js

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,7 +1,13 @@
 const allowedOrigins=require('./allowedOrigins')
 
 const corsOptions={
-    origin:"https://lovely-griffin-6978bb.netlify.app",
+      origin: function (origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
     optionsSuccessStatus:200,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['X-PINGOTHER', 'Content-Type',"Authorization","Origin", 'HEAD', 'OPTIONS',"Accept","Cache-Control",'Cookie','X-Requested-With'],
